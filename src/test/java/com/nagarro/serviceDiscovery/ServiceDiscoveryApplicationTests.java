901c6202cf68d970ae22777c16ef2a95aa2c8969
Rohit Sharma <rohit.sharma04@nagarro.com>
2021-03-01 13:21:09 +0530

    service-discovery implementation

---
+++ src/test/java/com/nagarro/serviceDiscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.serviceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
